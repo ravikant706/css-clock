16cd7b6ab2aea50c5d60cc6936eb08b084b3d112
ravikant706 <ravikant706@gmail.com>
2024-01-16 23:51:44 +0530

    second-hand work proper

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-document.getElementById('demo').innerHTML = "hello world"
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand')
+const hourHand = document.querySelector('.hour-hand')
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+}
+
+setInterval(setDate, 1000);
+setDate();
