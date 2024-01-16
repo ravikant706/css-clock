61eacce9d49a85da6db40c7bc686654ee3308c36
ravikant706 <ravikant706@gmail.com>
2024-01-16 23:54:14 +0530

    minute and hour hand also work proper

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ function setDate() {
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360);
+    hourHand.style.transform = `rotate(${hourHand}deg)`;
 }
 
 setInterval(setDate, 1000);
